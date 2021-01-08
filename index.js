6cab11fd1ccd7df28dd2d724f194553617e6c2ef
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-01-08 12:16:17 +0100

    Debut, justification du texte

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const bodyParser = require('body-parser')
 app = express()
 
 app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.text())
+
+
+app.post('/api/justify', (request, response)=>{
+    response.setHeader('Content-Type', 'text/plain');
+
+    response.send(justify(request.body.slice()))
+})
 
 app.listen(8000, ()=>console.log('Started on port 8080'))
+
+var justify= (text)=>{
+
+    var justify_text = text.split(' ')
+
+    return justify_text
+}
